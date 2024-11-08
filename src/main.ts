abd393df95cdb670d32d94099e4a184544d56948
Lisach0w0 <100955567+Lisach0w0@users.noreply.github.com>
2024-11-08 21:07:41 +0000

    簡単なfirebase処理とルーティング追加

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,21 @@ import { createApp } from "vue";
 // import "./style.css";
 import "bulma/css/bulma.min.css";
 import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import PointChecker from "./views/PointCheckerView.vue";
+import Organizer from "./views/OrganizerView.vue";
+import Register from "./views/RegisterView.vue";
+import {firebaseConfig } from "./config/firebase";
+import { initializeApp } from "firebase/app";
+initializeApp(firebaseConfig);
 
-createApp(App).mount("#app");
+console.log(firebaseConfig);
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "home", component: PointChecker },
+    { path: "/organizer", name: "page1", component: Organizer },
+    { path: "/register", name: "page2", component: Register },
+  ],
+});
+createApp(App).use(router).mount("#app");
