840b90f8552de3d769559f89309f9ba6d977c5fa
Lisach0w0 <100955567+Lisach0w0@users.noreply.github.com>
2024-11-08 08:12:54 +0000

    firebaseの定義を追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+// TODO firebase呼び出し
+// firestore接続
+// 仮で特定のランク設定取得
+
+
+// テスト用でランクポイントの計算表json仮設定
+// ランク表定義
+// 規定ポイント定義
+// メンバー追加
+// メンバーのランクポイント計算(watchしたいなぁ)
 </script>
 
 <template>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly FIREBASE_API_KEY: string
+    readonly FIREBASE_AUTH_DOMAIN: string
+    readonly FIREBASE_PROJECT_ID: string
+    readonly FIREBASE_STORAGE_BUCKET: string
+    readonly FIREBASE_MESSAGING_ID: string
+    readonly FIREBASE_APP_ID: string
+    readonly FIREBASE_MEASUREMENT_ID: string
+    // その他の環境変数...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
