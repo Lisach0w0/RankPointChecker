232c1104755ee664103409be4ca2ac13742e9af2
Lisach0w0 <100955567+Lisach0w0@users.noreply.github.com>
2024-11-15 16:48:11 +0000

    organizerにログインチェックだけ追加

--- src/views/OrganizerView.vue
+++ src/views/OrganizerView.vue
@@ -1,6 +1,39 @@
 <script setup lang="ts">
-// ユーザー情報取得
+import { getAuth, User } from 'firebase/auth';
+import { onMounted, reactive } from 'vue';
 
+type State = {
+  isLoading: boolean;
+  isLogin: boolean;
+  user: User | undefined;
+  message: string;
+}
+
+const state = reactive<State>({
+  isLoading: true,
+  isLogin: false,
+  user: undefined,
+  message: "",
+});
+
+// ユーザー情報取得
+const auth = getAuth();
+
+onMounted(() => {
+  state.isLoading = true;
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      state.user = user;
+      state.isLogin = true;
+      console.log("uid", user.uid);
+    } else {
+      console.log("not login");
+      state.message="ログインできていません。最初に戻ってもう一度ログインしてください。";
+      state.isLoading = false;
+    }
+  });
+  if(!state.isLoading) return;
+});
 // firestoreの自分の作ったものリスト取得
 
 // ユーザー情報更新処理
